37d49f040b299dfb6482f76e770f47b4c07494c2
Alexander Pajer <alexander.pajer@integrations.at>
2019-06-13 23:08:37 +0200

    effects todo

--- 08 State/VouchersNGRX/src/app/demos/state/demo.effects.ts
+++ 08 State/VouchersNGRX/src/app/demos/state/demo.effects.ts
@@ -1,42 +1,39 @@
-import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { VouchersService } from '../../vouchers/voucher.service';
-import * as demoActions from './demo.actions.creator';
-import { Voucher } from '../../shared/model/model';
+import { Injectable } from "@angular/core";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { Action } from "@ngrx/store";
+import { Observable, of } from "rxjs";
+import { catchError, map, mergeMap } from "rxjs/operators";
+import { VouchersService } from "../../vouchers/voucher.service";
+import * as demoActions from "./demo.actions.creator";
+import { Voucher } from "../../shared/model/model";
 
 @Injectable()
 export class VouchersEffects {
-	constructor(private actions$: Actions, private vs: VouchersService) {}
+  constructor(private actions$: Actions, private vs: VouchersService) {}
 
-	@Effect()
-	loadVouchers$: Observable<Action> = this.actions$.pipe(
-		ofType(demoActions.DemoActionTypes.Load),
-		mergeMap((action) =>
-			this.vs
-				.getVouchers()
-				.pipe(
-					map((vouchers: Voucher[]) => new demoActions.LoadSuccess(vouchers)),
-					catchError((err) => of(new demoActions.LoadErr(err)))
-				)
-		)
-	);
+  //TODO: Notice Effects for Vouchers
+  @Effect()
+  loadVouchers$: Observable<Action> = this.actions$.pipe(
+    ofType(demoActions.DemoActionTypes.Load),
+    mergeMap(action =>
+      this.vs.getVouchers().pipe(
+        map((vouchers: Voucher[]) => new demoActions.LoadSuccess(vouchers)),
+        catchError(err => of(new demoActions.LoadErr(err)))
+      )
+    )
+  );
 
-	@Effect()
-	deleteVoucher$: Observable<Action> = this.actions$.pipe(
-		ofType(demoActions.DemoActionTypes.DeleteVoucher),
-		map((action: demoActions.DeleteVoucher) => action.payload),
-		mergeMap((vid: number) =>
-			this.vs
-				.deleteVoucher(vid)
-				.pipe(
-					map(() => new demoActions.DeleteVoucherSuccess(vid)),
-					catchError((err) => of(new demoActions.DeleteVoucherErr(err)))
-				)
-		)
-	);
+  @Effect()
+  deleteVoucher$: Observable<Action> = this.actions$.pipe(
+    ofType(demoActions.DemoActionTypes.DeleteVoucher),
+    map((action: demoActions.DeleteVoucher) => action.payload),
+    mergeMap((vid: number) =>
+      this.vs.deleteVoucher(vid).pipe(
+        map(() => new demoActions.DeleteVoucherSuccess(vid)),
+        catchError(err => of(new demoActions.DeleteVoucherErr(err)))
+      )
+    )
+  );
 
-	//Todo: Implement Effects for: create, update
+  //TODO: Implement Effects for: create, update
 }
