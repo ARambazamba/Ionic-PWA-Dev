8b2f117f46f4c0ebda89ed32863b66a3a2959297
Alexander Pajer <alexander.pajer@integrations.at>
2019-06-13 17:16:20 +0200

    auth restructure

--- 06 Ionic/06 Security/SecurityFirebase/src/app/auth/register/register.component.ts
+++ 06 Ionic/06 Security/SecurityFirebase/src/app/auth/register/register.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertService } from "src/app/shared/alert.service";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  constructor(public as: AlertService, public auth: AuthService) {}
 
-  constructor() { }
+  reg = {
+    displayName: "",
+    email: "",
+    pwd: "",
+    pwd2: ""
+  };
 
   ngOnInit() {}
 
+  registerAcct() {
+    if (this.reg.pwd != this.reg.pwd2) {
+      this.as.displayAlert("Passwords", "Your passwords dont match");
+      this.reg.pwd = "";
+      this.reg.pwd2 = "";
+    } else {
+      if (this.reg.displayName == "") {
+        this.reg.displayName = this.reg.email;
+      }
+
+      this.auth
+        .createUser(this.reg.email, this.reg.pwd)
+        .then((user: firebase.User) => {
+          this.as.displayAlert(user.email, "Acct created - Please Login");
+        });
+    }
+  }
 }
