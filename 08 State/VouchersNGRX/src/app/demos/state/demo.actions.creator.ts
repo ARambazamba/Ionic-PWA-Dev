239b76f46734cdfb1f770b645af673fe6ab68057
Alexander Pajer <alexander.pajer@integrations.at>
2019-06-13 23:07:26 +0200

    todo

--- 08 State/VouchersNGRX/src/app/demos/state/demo.actions.creator.ts
+++ 08 State/VouchersNGRX/src/app/demos/state/demo.actions.creator.ts
@@ -1,89 +1,89 @@
-import { Action } from '@ngrx/store';
-import { Voucher } from '../../shared/model/model';
+import { Action } from "@ngrx/store";
+import { Voucher } from "../../shared/model/model";
 
-//Create Enum for Action Types
+//TODO: Notice Enum for Action Types
 
 export enum DemoActionTypes {
-	ToggleShowFilter = '[Demo] Toggle Show Filter',
-	UpdateFood = '[Demo] Update Food',
-	Load = '[Vouchers] Load',
-	LoadSuccess = '[Vouchers] Load Success',
-	LoadErr = '[Vouchers] Load Err',
-	SetCurrentVoucher = '[Voucher] SetCurrentVoucher',
-	ClearCurrentVoucher = '[Voucher] ClearCurrentVoucher',
-	InitializeVoucher = '[Voucher] InitializeVoucher',
-	DeleteVoucher = '[Voucher] DeleteVoucher',
-	DeleteVoucherSuccess = '[Voucher] DeleteVoucher Success',
-	DeleteVoucherErr = '[Voucher] DeleteVoucher Err'
+  ToggleShowFilter = "[Demo] Toggle Show Filter",
+  UpdateFood = "[Demo] Update Food",
+  Load = "[Vouchers] Load",
+  LoadSuccess = "[Vouchers] Load Success",
+  LoadErr = "[Vouchers] Load Err",
+  SetCurrentVoucher = "[Voucher] SetCurrentVoucher",
+  ClearCurrentVoucher = "[Voucher] ClearCurrentVoucher",
+  InitializeVoucher = "[Voucher] InitializeVoucher",
+  DeleteVoucher = "[Voucher] DeleteVoucher",
+  DeleteVoucherSuccess = "[Voucher] DeleteVoucher Success",
+  DeleteVoucherErr = "[Voucher] DeleteVoucher Err"
 }
 
-//Implement Action Creators
+//TODO: Implement Action Creators
 
 export class ToggleShowFilter implements Action {
-	readonly type = DemoActionTypes.ToggleShowFilter;
-	constructor(public payload: boolean) {}
+  readonly type = DemoActionTypes.ToggleShowFilter;
+  constructor(public payload: boolean) {}
 }
 
 export class UpdateFood implements Action {
-	readonly type = DemoActionTypes.UpdateFood;
-	constructor(public payload: string) {}
+  readonly type = DemoActionTypes.UpdateFood;
+  constructor(public payload: string) {}
 }
 
 export class Load implements Action {
-	readonly type = DemoActionTypes.Load;
+  readonly type = DemoActionTypes.Load;
 }
 
 export class LoadSuccess implements Action {
-	readonly type = DemoActionTypes.LoadSuccess;
-	constructor(public payload: Voucher[]) {}
+  readonly type = DemoActionTypes.LoadSuccess;
+  constructor(public payload: Voucher[]) {}
 }
 
 export class LoadErr implements Action {
-	readonly type = DemoActionTypes.LoadErr;
-	constructor(public payload: string) {}
+  readonly type = DemoActionTypes.LoadErr;
+  constructor(public payload: string) {}
 }
 
 export class SetCurrentVoucher implements Action {
-	readonly type = DemoActionTypes.SetCurrentVoucher;
-	constructor(public payload: number) {}
+  readonly type = DemoActionTypes.SetCurrentVoucher;
+  constructor(public payload: number) {}
 }
 
 export class ClearCurrentVoucher implements Action {
-	readonly type = DemoActionTypes.ClearCurrentVoucher;
+  readonly type = DemoActionTypes.ClearCurrentVoucher;
 }
 
 export class DeleteVoucher implements Action {
-	readonly type = DemoActionTypes.DeleteVoucher;
-	constructor(public payload: number) {}
+  readonly type = DemoActionTypes.DeleteVoucher;
+  constructor(public payload: number) {}
 }
 
 export class DeleteVoucherSuccess implements Action {
-	readonly type = DemoActionTypes.DeleteVoucherSuccess;
+  readonly type = DemoActionTypes.DeleteVoucherSuccess;
 
-	constructor(public payload: number) {}
+  constructor(public payload: number) {}
 }
 
 export class DeleteVoucherErr implements Action {
-	readonly type = DemoActionTypes.DeleteVoucherErr;
+  readonly type = DemoActionTypes.DeleteVoucherErr;
 
-	constructor(public payload: string) {}
+  constructor(public payload: string) {}
 }
 
 export class InitializeVoucher implements Action {
-	readonly type = DemoActionTypes.InitializeVoucher;
+  readonly type = DemoActionTypes.InitializeVoucher;
 }
 
-// Create Union Type to serve as a second param for Reducer
+//TODO:  Create Union Type to serve as a second param for Reducer
 
 export type DemoActions =
-	| ToggleShowFilter
-	| UpdateFood
-	| SetCurrentVoucher
-	| ClearCurrentVoucher
-	| InitializeVoucher
-	| Load
-	| LoadSuccess
-	| LoadErr
-	| DeleteVoucher
-	| DeleteVoucherSuccess
-	| DeleteVoucherErr;
+  | ToggleShowFilter
+  | UpdateFood
+  | SetCurrentVoucher
+  | ClearCurrentVoucher
+  | InitializeVoucher
+  | Load
+  | LoadSuccess
+  | LoadErr
+  | DeleteVoucher
+  | DeleteVoucherSuccess
+  | DeleteVoucherErr;
